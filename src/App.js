cedaf1bc8cd3a6973de951dcb27b01e8dbef4cdd
Duncan Smith <duncanssmith@gmail.com>
2021-11-11 13:52:31 +0000

    README note

--- src/App.js
+++ src/App.js
@@ -1,52 +1,65 @@
 import React, { useState } from "react";
 import Axios from "axios";
-
 import "./App.css";
 
 function App() {
 
-  // const [count, setCount] = useState("")
   const [weather, setWeather] = useState("")
-  const example = [1,2,3,4,5,6]
+
+  //  Axios.get("https://api.openweathermap.org/data/2.5/weather?q=London&mode=json&units=metric&appid=786986c790691a6fe26c60fcd9fae106").then((response)=>{
 
   const getWeather = () => {
-    Axios.get(API_URL).then((response)=>{
-      console.log(response);
+    Axios.get("https://api.openweathermap.org/data/2.5/forecast?q=London&mode=json&units=metric&appid=786986c790691a6fe26c60fcd9fae106").then((response)=>{
 
-      // clone the response
-      // const data = [...example]
-      // const items = example.slice()
-      const items = [].concat(response.data) 
+      // console.log(response);
+      // console.log(response.data.list);
 
-      // console.log("ITEMS: " + response.data)
+      const items = [].concat(response.data.list) 
+      const results = []
+      const loc = response.data.city.name + " " + response.data.city.country
 
       items.forEach(function(item){
-          console.log('Duncan ' + item.city.name)
+          // console.log('At ' + item.dt_txt)
+          // console.log('Clouds ' + item.clouds.all)
+          // console.log('Temp ' + item.main.temp)
+          // console.log('Wind ' + item.wind.speed)
+          const result = []
+          // result.push('{"At": "' + '"' + item.dt_txt + '"')
+          // result.push('"Clouds": ' + '"' + item.clouds.all + '"')
+          // result.push('"Temp": ' + '"' + item.main.temp + '"')
+          // result.push('"Wind": ' + '"' + item.wind.speed + '"}')
+          result.push('At: ' + item.dt_txt )
+          result.push('Clouds: ' +  item.clouds.all )
+          result.push('Temp: ' + item.main.temp )
+          result.push('Wind: ' + item.wind.speed )
+          results.push(result)
       })
 
-      setWeather("City: " + response.data.city.name + ", " +  response.data.city.country + ", " +
-          " Datetime: " + response.data.list[0].dt_txt  + " " +
-          " Clouds: " + response.data.list[0].clouds.all + "%, " +
-          " Desc: " + response.data.list[0].weather[0].description +
-          " Temp: " + response.data.list[0].main.temp + " deg C " +
-          " Temp min: " + response.data.list[0].main.temp_min + " deg C " +
-          " Temp max: "  + response.data.list[0].main.temp_max + " deg C " +
-          " Humidity: " + response.data.list[0].main.humidity + "% " +
-          " Wind direction: " + response.data.list[0].wind.deg + " degrees " +
-          " Wind speed: " + response.data.list[0].wind.speed + " " +
-          " Count of data points: " + response.data.cnt)
-      // setWeather(response.data.list)
-      // setCount(response.data.cnt)
+      // console.log(loc)
+      console.log(results)
+
+      // console.log(results.map((item) => <li>{item}</li>))
+
+      // const mappedResults = results.map((item) => <li>{item}</li>)
+      let weatherString = ''
+
+      results.forEach(function(dataPoint){
+        weatherString = weatherString + '<td>' + dataPoint[0] + '</td><td>' + dataPoint[1] + '</td><td>' + dataPoint[2] + '</td>'
+      })
+
+      // console.log(mappedResults)
+
+      setWeather(loc + " " + results)
+
     });
   }
-  console.log("DS DEBUG");
+
   return <div>
-    Hello World <button onClick={getWeather}>Get weather right now</button>
-    <br/>
+    <button onClick={getWeather}>Get weather</button>
     <hr/>
-    {/* {count} */}
-    {weather}
-    <hr/>
+    { weather }
+
+        <hr/>
     </div>;
 }
 
